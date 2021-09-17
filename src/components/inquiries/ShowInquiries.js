4ef3e2816620fd493deac183d2a379e438d270e8
jbeecy <john.e.beecy@gmail.com>
2021-09-17 11:52:27 -0400

    add functional delete
    This commit adds a functional delete button with working history.push

--- src/components/inquiries/ShowInquiries.js
+++ src/components/inquiries/ShowInquiries.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
-import { showInquiries } from '../../api/inquiries'
-import { Link } from 'react-router-dom'
+import { showInquiries, deleteInquiry } from '../../api/inquiries'
+// import { Link } from 'react-router-dom'
+import { Card, Button } from 'react-bootstrap'
 
 class ShowInquiries extends Component {
   constructor (props) {
@@ -21,14 +22,67 @@ class ShowInquiries extends Component {
       .catch(err => msgAlert({ heading: 'Index failed :(', message: 'Something went wrong: ' + err.message, variant: 'danger' }))
   }
 
+  handleDelete = (event) => {
+    const { msgAlert, user, history } = this.props
+    event.preventDefault()
+    const inquiryDataId = event.target.attributes.getNamedItem('data-id').value
+    deleteInquiry(inquiryDataId, user)
+      .then(() =>
+        msgAlert({
+          heading: 'Inquiry deleted.',
+          message: 'Your inquiry has been successfully deleted.',
+          variant: 'success'
+        })
+      )
+      .then(() => history.push('/inquiries'))
+      .then(() => showInquiries(user)
+        .then((response) => this.setState({
+          inquiries: response.data.inquiries
+        }))
+        .then(() => msgAlert({
+          heading: 'Success!',
+          message: 'Here are the inquiries again.',
+          variant: 'success'
+        })))
+      .catch((err) =>
+        msgAlert({
+          heading: 'Delete failed.',
+          message: 'Unable to delete. If you are the owner you cannot delete this.' + err.message,
+          variant: 'danger'
+        })
+      )
+  }
+
   render () {
+    const cardContainerLayout = {
+      display: 'flex',
+      justifyContent: 'center',
+      flexFlow: 'row wrap'
+    }
+    const { history } = this.props
     const inquiries = this.state.inquiries.map(inquiry => (
-      <li key={inquiry._id}>
-        <Link to={ `/inquiries/${inquiry._id}` }>{inquiry.subject}</Link>
-      </li>
+      <Card key={inquiry._id} style={{ width: '45rem' }}>
+        <Card.Body>
+          <Card.Title>{inquiry.subject}</Card.Title>
+          <Card.Text>{inquiry.content}</Card.Text>
+          <Button
+            style={{ margin: '10px', alignItems: 'center' }}
+            onClick={() => history.push(`/inquiries/${inquiry._id}`)}
+          >
+            Update Inquiry
+          </Button>
+          <Button
+            variant='btn btn-outline-danger'
+            onClick={this.handleDelete}
+            data-id={inquiry._id}
+            className='button'>
+            Delete Inquiry
+          </Button>
+        </Card.Body>
+      </Card>
     ))
     return (
-      <div>
+      <div style={cardContainerLayout}>
         <h4>Inquiries</h4>
         <ul>
           {inquiries}
