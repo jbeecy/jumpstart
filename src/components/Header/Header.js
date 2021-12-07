ee70ce33b547b1ee3b0f0a95749dd39f21234e02
jbeecy <john.e.beecy@gmail.com>
2021-12-07 12:19:29 -0500

    change the navbar color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
+  <Navbar bg='dark' variant='dark' expand='md'>
     <Navbar.Brand>
       <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>Welcome to Jumpstart!</Link>
     </Navbar.Brand>
